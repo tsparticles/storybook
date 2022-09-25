fc2704b4f81b7d17c5100da39c26fa425c8d9a16
Matteo Bruni <176620+matteobruni@users.noreply.github.com>
2022-09-25 16:15:44 +0200

    build: adding CI

--- src/stories/Particles.jsx
+++ src/stories/Particles.jsx
@@ -1,10 +1,16 @@
 import React from 'react';
 import Particles from 'react-particles';
 import PropTypes from 'prop-types';
+import { loadFull } from "tsparticles";
 
 export const ParticlesComponent = ({ backgroundColor, particlesColor }) => {
+    const particlesInit = async (engine) => {
+        await loadFull(engine);
+    };
+
     return (
         <Particles
+            init={particlesInit}
             options={{
                 background: {
                     color: {
