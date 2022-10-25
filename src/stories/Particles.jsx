f8b267117abc9d5484f8c09d774934be708f8bd7
Enhmunh-E <enhuush0704@gmail.com>
2022-10-25 22:36:07 +0800

    Particles story improvement

--- src/stories/Particles.jsx
+++ src/stories/Particles.jsx
@@ -1,46 +1,52 @@
-import React from 'react';
-import ReactParticles from 'react-particles';
-import PropTypes from 'prop-types';
+import React from "react";
+import ReactParticles from "react-particles";
+import PropTypes from "prop-types";
 import { loadFull } from "tsparticles";
 
-export const Particles = ({ backgroundColor, particlesColor, links }) => {
-    const particlesInit = async (engine) => {
-        await loadFull(engine);
-    };
+export const Particles = ({
+  backgroundColor,
+  particlesColor,
+  links,
+  linkColor,
+}) => {
+  const particlesInit = async (engine) => {
+    await loadFull(engine);
+  };
 
-    return (
-        <ReactParticles
-            init={particlesInit}
-            options={{
-                background: {
-                    color: {
-                        value: backgroundColor,
-                    }
-                },
-                particles: {
-                    color: {
-                        value: particlesColor,
-                    },
-                    links: {
-                        enable: links
-                    },
-                    move: {
-                        enable: true,
-                    }
-                }
-            }}
-        />
-    );
+  return (
+    <ReactParticles
+      init={particlesInit}
+      options={{
+        background: {
+          color: {
+            value: backgroundColor,
+          },
+        },
+        particles: {
+          color: {
+            value: particlesColor,
+          },
+          links: {
+            enable: links,
+            color: linkColor,
+          },
+          move: {
+            enable: true,
+          },
+        },
+      }}
+    />
+  );
 };
 
 Particles.propTypes = {
-    backgroundColor: PropTypes.string,
-    links: PropTypes.bool,
-    particlesColors: PropTypes.arrayOf(PropTypes.string),
+  backgroundColor: PropTypes.string,
+  links: PropTypes.bool,
+  particlesColors: PropTypes.arrayOf(PropTypes.string),
 };
 
 Particles.defaultProps = {
-    backgroundColor: "#000",
-    links: true,
-    particlesColors: [ "#fff" ]
+  backgroundColor: "#000",
+  links: true,
+  particlesColors: ["#fff"],
 };
